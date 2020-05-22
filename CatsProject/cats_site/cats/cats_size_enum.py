68b3923baa205e4fecf49f82776ffd6c4446cb6d
daAnis <daria.anisimowa@yandex.ru>
2020-05-22 19:57:37 +0400

    Скорректирован вид отображения изображений

---
+++ CatsProject/cats_site/cats/cats_size_enum.py
@@ -0,0 +1,22 @@
+dict_cats_styles = {
+    'bambino' : 'photo-bambino',
+    'donskoysfincks' : 'photo-bambino',
+    'CanadskiySfinks' : 'photo-canada',
+    'elf' : 'photo',
+    'abissinskaya' : 'photo',
+    'bengal' : 'photo',
+    'british' : 'photo-brit',
+    'mau' : 'photo',
+    'manchkin' : 'photo-canada',
+    'russianblue' : 'photo',
+    'snowshu' : 'photo',
+    'tonkin' : 'photo',
+    'angor' : 'photo-angor',
+    'neva' : 'photo',
+    'nordvay' : 'photo-norv',
+    'persia' : 'photo',
+    'birma' : 'photo_birma',
+    'reks' : 'photo-reks',
+    'siberian' : 'photo',
+    'shartrez' : 'photo-norv'
+}
